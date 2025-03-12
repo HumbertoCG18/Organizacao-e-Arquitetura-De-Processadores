5157c86a455c0b0591bdb5a61a731717addb0017
Humberto Corrêa <humberto.cgomes17@gmail.com>
2025-03-12 17:55:22 -0300

    Create macro3.c

---
+++ March/12/macro3.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int func_sum (int a, int b){
+    return a+b;
+}
+
+#define macro_sum(a, b) a+b+c;
+
+int main() {
+    char array[] = {"pucrs"};
+    char *p = i&array[0];
+    
+    printf("%x -> %c \n", &p, p);
+    printf("%x -> %c \n", &p, p);
+}
